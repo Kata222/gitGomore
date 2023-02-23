d38fc5e647caa82669dbdb5293fac1563b093f2f
Katarzyna <catherinechlap@gmail.com>
2023-02-23 16:28:56 +0100

    Initial Commit

--- search_city_test.py
+++ search_city_test.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
 
 driver.implicitly_wait(10)
 driver.maximize_window()
 driver.get("https://gomore.se/")
 
-driver.find_element_by_xpath("//span[text()='Acceptera alla']").click()
-driver.find_element_by_xpath("/html/body/section[1]/div[2]/div/div/div[1]/div/div[1]").click()
-driver.find_element_by_xpath("//div//input").send_keys('Malmö')
+driver.find_element("xpath","//span[text()='Acceptera alla']" ).click()
+driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div[1]/div/div[1]").click()
+driver.find_element("xpath","//div//input").send_keys('Malmö')
+
+
+
 
 
 
