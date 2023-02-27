1b327b0f71bb995726a78eac0d528241ff10b7f5
Katarzyna <catherinechlap@gmail.com>
2023-02-27 17:53:25 +0100

    Checking if the list of cars is correct

---
+++ correctness_of_cars_names.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+
+driver.implicitly_wait(10)
+driver.maximize_window()
+driver.get("https://gomore.se/")
+
+driver.find_element("xpath","//span[text()='Acceptera alla']").click()
+
+driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div[1]/div/div[1]").click()
+driver.find_element("xpath","//div//input").send_keys('Malmö')
+driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[1]/div/div[2]/div[1]").click()
+time.sleep(6)
+
+driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[3]/div[1]/div[1]/div[2]/div/div[3]/div[27]/span").click()
+driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[3]/div[1]/div[2]/div[2]/div/div/li[8]/span").click()
+time.sleep(6)
+
+driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[3]/div[3]/div[1]/div[2]/div/div[3]/div[28]/span").click()
+driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[3]/div[3]/div[2]/div[2]/div/div/li[33]/span").click()
+
+time.sleep(6)
+driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/button").click()
+
+cars = driver.find_elements("xpath","//div[contains(@class,'js-results')]//h2")
+car_names = [car.get_attribute("textContent") for car in cars]
+for name in car_names:
+    print("Car name: " + name)
+
+assert car_names[0] == 'Citroën C3 Aircross'
+#assert car_names[1] == 'Ford Transit Custom'
+#assert car_names[2] == 'BMW 1 Series'
+#assert car_names[3] == 'Tesla Model Y Long Range'
+#assert car_names[4] == 'Toyota AYGO'
+#assert car_names[5] == 'Kia Ceed'
+#assert car_names[6] == 'Opel VIVARO'
+#assert car_names[7] == 'Hyundai Kona Electric'
+#assert car_names[8] == 'Volkswagen Polo'
+#assert car_names[9] == 'Volvo V90'
+
+driver.quit()
