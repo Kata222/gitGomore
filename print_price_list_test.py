b629cdd023e0b360e673da7be1dde2d69839de17
Katarzyna <catherinechlap@gmail.com>
2023-02-27 13:12:01 +0100

    Prices test

--- print_price_list_test.py
+++ print_price_list_test.py
@@ -14,16 +14,16 @@ driver.find_element("xpath","//span[text()='Acceptera alla']").click()
 driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div[1]/div/div[1]").click()
 driver.find_element("xpath","//div//input").send_keys('Malmö')
 driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[1]/div/div[2]/div[1]").click()
-time.sleep(5)
+time.sleep(6)
 
 driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[3]/div[1]/div[1]/div[2]/div/div[3]/div[27]/span").click()
 driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[3]/div[1]/div[2]/div[2]/div/div/li[8]/span").click()
-time.sleep(5)
+time.sleep(6)
 
 driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[3]/div[3]/div[1]/div[2]/div/div[3]/div[28]/span").click()
 driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/div[3]/div[3]/div[2]/div[2]/div/div/li[33]/span").click()
 
-time.sleep(5)
+time.sleep(6)
 driver.find_element("xpath","/html/body/section[1]/div[2]/div/div/div/button").click()
 
 cars = driver.find_elements("xpath","//div[contains(@class,'js-results')]//h2")
